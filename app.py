4c228fa9e681908dd95f07e272a36a15e1886038
tim <tim.j.phillips10@gmail.com>
2020-12-20 11:00:48 -0500

    Added html preview feature and favicon

--- app.py
+++ app.py
@@ -8,7 +8,12 @@ app = dash.Dash(__name__,
         'src':'https://kit.fontawesome.com/12a2550f5c.js',
         'crossorigin': 'anonymous'
     }],
-    meta_tags=[{'name': 'viewport', 'content': 'width=device-width, initial-scale=1'}],
+    meta_tags=[
+        {'name':'The Bourbonhuntr', 'content':'Virginias Bourbon Tracker'},
+        {'name': 'viewport', 'content': 'width=device-width, initial-scale=1'},
+        {'http-equiv': 'X-UA-Compatible', 'content': 'IE=edge'},
+        {'property':'og:description','content':'The Bourbonhuntr provides daily information and analytics on the availability of certain bourbons across Virginia.'},
+        {'property':'og:image', 'content':'https://app.bourbonhuntr.com/assets/TheBourbonHuntr_Logo_v1.png'}],
     suppress_callback_exceptions=True)
 
 app.title = 'The Bourbonhuntr'

13f76d200c62a9662758b3ad5f2bc7309cbed320
tim <tim.j.phillips10@gmail.com>
2022-01-23 12:23:40 -0500

    Removed environments folder from repo

--- environment/settings.py
+++ environment/settings.py
@@ -2,7 +2,7 @@ import os
 from os.path import join, dirname
 from dotenv import load_dotenv
 
-dotenv_path = join(dirname(__file__), ".env.dev") 
+dotenv_path = join(dirname(__file__), ".env") 
 
 load_dotenv(dotenv_path=dotenv_path, override=True)
 
